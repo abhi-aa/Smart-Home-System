38d40941b0a33afb0fd32299400141a7b79c9683
abhi-aa <151111839+abhi-aa@users.noreply.github.com>
2024-08-24 13:07:59 -0400

    Delete Automated_Blinds/manual_only directory

--- Automated_Blinds/manual_only/credentials.h
+++
@@ -1,4 +0,0 @@
-// credentials.h
-#define SECRET_SSID "BELL123" // Your Wi-Fi SSID
-#define SECRET_PASS "Panipuri%034" // Your Wi-Fi Password
-#define FIREBASE_AUTH "8MqmPGcSgPv2azl8Y8FvfqdorK5HIZke7AjKrumd" // Your Firebase Auth Token
